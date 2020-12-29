14c44720c3e4decf44c3436138aecf50beee1dd7
puppet-666 <1063771217@qq.com>
2020-12-29 09:53:36 +0800

    fix: remove ui component

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import { Message, Confirm } from '@ss/mtd-vue';
-
-// // 引入 MTDUI 2.0 样式
-// import '@ss/mtd-vue/lib/theme2/index.css'
-
-// // 引入组件库
-// import MTD from '@ss/mtd-vue'
-
-
-// Vue.use(MTD)
-// Vue.prototype.$mtd = {
-//   message: Message,
-//   confirm: Confirm,
-// };
 
 new Vue({
   el: '#app',
